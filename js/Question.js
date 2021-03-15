4b58ebf24bbb0e9ffd032419e6e87d7d90ff02a6
Usman Ali <52999667+UsmanAli27@users.noreply.github.com>
2021-03-15 16:58:42 +0500

    Update Question.js

--- js/Question.js
+++ js/Question.js
@@ -1,9 +1,10 @@
-function Question(text, choices, answer){
-    this.text = text;
-    this.choices = choices;
-    this.answer = answer;
+function Question(text, choices, answer) {
+  this.text = text;
+  this.choices = choices;
+  this.answer = answer;
+  //comment added
 }
 
-Question.prototype.correctAnswer = function(choice){
-    return choice === this.answer;
-}
+Question.prototype.correctAnswer = function (choice) {
+  return choice === this.answer;
+};
